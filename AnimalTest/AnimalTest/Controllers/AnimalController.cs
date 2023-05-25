45ca93bda47bf2e79bf271af342c43808c40c823
Andrija Kruhoberec <akruhoberec1@outlook.com>
2023-05-25 18:36:02 +0200

    Fixed responses for all methods

--- AnimalTest/AnimalTest/Controllers/AnimalController.cs
+++ AnimalTest/AnimalTest/Controllers/AnimalController.cs
@@ -15,18 +15,18 @@ namespace AnimalTest.Controllers
 
 
 
-        public static List<Animal> animals = new List<Animal>();
-        //    {
-        //       new Animal { Id=1, Name="Dog", Sound="VauVau" },
-        //       new Animal { Id=2, Name="Turtle", Sound="Hellooo" },
-        //       new Animal { Id=3, Name="Fish", Sound=" " },
-        //       new Animal { Id=4, Name="Cat", Sound="Meow" },
-        //       new Animal { Id=5, Name="Cow", Sound="Mooo!" }
-        //    };
+        public static List<Animal> animals = new List<Animal>
+            {
+               new Animal { Id=1, Name="Dog", Sound="VauVau" },
+               new Animal { Id=2, Name="Turtle", Sound="Hellooo" },
+               new Animal { Id = 3, Name = "Fish", Sound = " " },
+               new Animal { Id = 4, Name = "Cat", Sound = "Meow" },
+               new Animal { Id = 5, Name = "Cow", Sound = "Mooo!" }
+            };
 
 
 
-        public HttpResponseMessage Get()
+public HttpResponseMessage Get()
         {
             if(animals.Count() == 0)
             {
@@ -40,7 +40,7 @@ namespace AnimalTest.Controllers
         {
             if(id < 0 || id > animals.Count())
             {
-                return Request.CreateResponse(HttpStatusCode.NotFound, $"Sorry, we cannot find an animal using id number {id}.");
+                return Request.CreateResponse(HttpStatusCode.NotFound, $"Sorry, we cannot find an animal using Id number {id}.");
             }
             Animal animal = animals.Find(m => m.Id == id);
             return Request.CreateResponse<Animal>(HttpStatusCode.OK, animal);
@@ -54,20 +54,20 @@ namespace AnimalTest.Controllers
             animal.Name = ani.Name;
             animal.Sound = ani.Sound;
 
-            if (animal == null)
+            if (animal.Id == 0 || animal.Name == null || animal.Id < 0)
             {
                 return Request.CreateResponse(HttpStatusCode.BadRequest, $"Sorry, we cannot post an animal");
             }
 
             animals.Add(animal);
 
-            return Request.CreateResponse<IEnumerable<Animal>>(HttpStatusCode.OK, animals);
+            return Request.CreateResponse<List<Animal>>(HttpStatusCode.OK, animals);
         }
 
 
         public HttpResponseMessage Put(int id, [FromBody]  Animal ani)
         {
-            if (id < 0 || id > animals.Count() || id != ani.Id)
+            if (id <= 0 || id > animals.Count())
             {
                 return Request.CreateResponse(HttpStatusCode.NotFound, $"Sorry, we cannot find an animal using id number {id}.");
             }
