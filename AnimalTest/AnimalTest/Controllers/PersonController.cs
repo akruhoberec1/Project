55ca8782659712e297b0ad8fef7bc057dd731131
akruhoberec1 <akruhoberec1@outlook.com>
2023-05-29 14:10:30 +0200

    početak geta

--- AnimalTest/AnimalTest/Controllers/PersonController.cs
+++ AnimalTest/AnimalTest/Controllers/PersonController.cs
@@ -20,35 +20,37 @@ namespace AnimalTest.Controllers
 
 
         //get method
-        //[Route("")]
-        //public HttpResponseMessage Get()
-        //{
-        //    List<Person> people = new List<Person>();
-        //    NpgsqlConnection connection = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["connectionString"].ToString());
-        //    connection.Open();
+        [Route("")]
+        public HttpResponseMessage Get()
+        {
+            List<Person> people = new List<Person>();
+            NpgsqlConnection connection = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["connectionString"].ToString());
+            connection.Open();
 
 
-        //    try
-        //    {
-        //        NpgsqlCommand cmd = new NpgsqlCommand($"SELECT * FROM Person ORDER BY LastName, FirstName", connection);
+            try
+            {
+                NpgsqlCommand cmd = new NpgsqlCommand($"SELECT * FROM Person ORDER BY LastName, FirstName", connection);
 
-        //        var reader = cmd.ExecuteReader();
-        //        if (reader.HasRows)
-        //        {
-        //            while (reader.Read())
-        //            {
-        //                Person person = new Person()
-        //                {
-   
-        //            }
-        //        }
-        //        return Request.CreateResponse(HttpStatusCode.NotFound, "No rows found.");
-        //    }
-        //    catch(Exception ex)
-        //    {
-        //        return Request.CreateResponse(HttpStatusCode.NotFound, ex);
-        //    }
-        //}
+                var reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        //popunimo listu objektima
+
+                        
+
+
+                    }
+                }
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "No rows found.");
+                }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, ex);
+            }
+        }
 
 
 
